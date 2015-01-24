4b33412f41d4f994e3028259531ffe5861c9fe56
Aaron Jacobs <aaron@aaronmjacobs.com>
2015-01-24 14:05:40 -0800

    Add Freefall level

---
+++ Assets/Scripts/Freefall/FloorScript.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorScript : MonoBehaviour {
+
+	public bool good;
+
+	private LevelController levelController;
+	private GameObject player1, player2, player3;
+	bool hitPlayer1, hitPlayer2, hitPlayer3;
+	bool reported;
+	int hitCount;
+
+	// Use this for initialization
+	void Start () {
+		GameObject levelControllerObj = GameObject.Find("LevelController");
+		if (levelControllerObj != null) {
+			levelController = levelControllerObj.GetComponent<LevelController>();
+		}
+	
+		player1 = GameObject.Find("Player1");
+		player2 = GameObject.Find("Player2");
+		player3 = GameObject.Find("Player3");
+
+		hitPlayer1 = hitPlayer2 = hitPlayer3 = false;
+		reported = false;
+		hitCount = 0;
+	}
+
+	void OnCollisionEnter2D(Collision2D other) {
+		if (!hitPlayer1 && other.gameObject == player1) {
+			hitPlayer1 = true;
+			++hitCount;
+		}
+
+		if (!hitPlayer2 && other.gameObject == player2) {
+			hitPlayer2 = true;
+			++hitCount;
+		}
+
+		if (!hitPlayer3 && other.gameObject == player3) {
+			hitPlayer3 = true;
+			++hitCount;
+		}
+
+		if (!reported && hitCount >= 2) {
+			reported = true;
+
+			if (levelController != null) {
+				if (good) {
+					levelController.OnWin(3.0f);
+				} else {
+					levelController.OnLose(3.0f);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Freefall/LogicScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LogicScript : MonoBehaviour {
+
+	private const float WIND_STRENGTH = 1.0f;
+	private const float PLAYER_STRENGTH = 1.2f;
+
+	private GameObject player1, player2, player3;
+
+	// Use this for initialization
+	void Start () {
+		player1 = GameObject.Find("Player1");
+		player2 = GameObject.Find("Player2");
+		player3 = GameObject.Find("Player3");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float windX = (Input.GetKey(KeyCode.RightArrow) ? WIND_STRENGTH : 0.0f) - (Input.GetKey(KeyCode.LeftArrow) ? WIND_STRENGTH : 0.0f);
+		Vector2 wind = new Vector2(windX, 0.0f);
+
+		float player1X = (Input.GetKey(KeyCode.D) ? PLAYER_STRENGTH : 0.0f) - (Input.GetKey(KeyCode.A) ? PLAYER_STRENGTH : 0.0f);
+		Vector2 player1Force = new Vector2(player1X, 0.0f);
+
+		//float player2X = (Input.GetKey(KeyCode.D) ? PLAYER_STRENGTH : 0.0f) - (Input.GetKey(KeyCode.A) ? PLAYER_STRENGTH : 0.0f);
+		Vector2 player2Force = new Vector2(0.0f, 0.0f);
+
+		//float player3X = (Input.GetKey(KeyCode.D) ? PLAYER_STRENGTH : 0.0f) - (Input.GetKey(KeyCode.A) ? PLAYER_STRENGTH : 0.0f);
+		Vector2 player3Force = new Vector2(0.0f, 0.0f);
+
+		player1.rigidbody2D.AddForce(wind + player1Force);
+		player2.rigidbody2D.AddForce(wind + player2Force);
+		player3.rigidbody2D.AddForce(wind + player3Force);
+	}
+}
