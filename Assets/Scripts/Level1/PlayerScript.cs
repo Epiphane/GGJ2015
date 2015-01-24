48592ca9bbc26c3300112d848adf70a0de6f9157
ElliotFiske <elliotfiske@gmail.com>
2015-01-24 12:25:38 -0800

    Box Level prototype done

--- Assets/Scripts/Level1/PlayerScript.cs
+++ Assets/Scripts/Level1/PlayerScript.cs
@@ -9,12 +9,34 @@ public class PlayerScript : MonoBehaviour {
 	void Start () {
 	
 	}
-	
+
+	public KeyCode up;
+	public KeyCode down;
+	public KeyCode left;
+	public KeyCode right;
+
 	// Update is called once per frame
 	void Update () {
-		float moveHorizontal = Input.GetAxis("Horizontal");
-		float moveVertical = Input.GetAxis("Vertical");
-		Vector3 translate = new Vector3(moveHorizontal, moveVertical, 0.0f) * Time.deltaTime * MOVE_SPEED;
-		transform.Translate(translate);
+//		float moveHorizontal = Input.GetAxis("Horizontal");
+//		float moveVertical = Input.GetAxis("Vertical");
+//		Vector3 translate = new Vector3(moveHorizontal, moveVertical, 0.0f) * Time.deltaTime * MOVE_SPEED;
+//		transform.Translate(translate);
+
+		if (Input.GetKey (up))
+		{
+			transform.Translate(Vector3.up * Time.deltaTime *4, Space.World);
+		}
+		if (Input.GetKey (down))
+		{    
+			transform.Translate(Vector3.down * Time.deltaTime *4, Space.World);
+		}  
+		if (Input.GetKey (right))
+		{
+			transform.Translate(Vector3.right* Time.deltaTime *4, Space.World);
+		}
+		if (Input.GetKey (left))
+		{    
+			transform.Translate(Vector3.left* Time.deltaTime *4, Space.World);
+		}  
 	}
 }
--- Assets/Scripts/Menu/MenuController.cs
+++ Assets/Scripts/Menu/MenuController.cs
@@ -3,7 +3,13 @@ using System.Collections;
 
 public class MenuController : MonoBehaviour {
 
+	public GameObject title;
+
 	public void LoadLevel(string name) {
 		GameController.instance.LoadLevel(name);
+	}
+
+	public void Start() {
+		title.GetComponent<Animator>().SetTrigger("hi");
 	}
 }
