491ef494e42b3b835a22299e86c970d76f0c4dcc
Aaron Jacobs <aaron@aaronmjacobs.com>
2015-01-23 23:24:49 -0800

    Add MenuController

---
+++ Assets/Scripts/Menu/MenuController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuController : MonoBehaviour {
+
+	public void LoadLevel(string name) {
+		GameController.instance.LoadLevel(name);
+	}
+}
