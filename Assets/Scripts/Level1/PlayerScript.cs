0312d632b7fb2d7ce96dead6051d722b625560e5
Epiphane <exyphnos@gmail.com>
2015-01-24 14:50:07 -0800

    Axis movement in boxgame

--- Assets/Scripts/Level1/PlayerScript.cs
+++ Assets/Scripts/Level1/PlayerScript.cs
@@ -10,42 +10,13 @@ public class PlayerScript : MonoBehaviour {
 	
 	}
 
-	public KeyCode up;
-	public KeyCode down;
-	public KeyCode left;
-	public KeyCode right;
+	public int player = 1;
 
 	// Update is called once per frame
 	void Update () {
-//		float moveHorizontal = Input.GetAxis("Horizontal");
-//		float moveVertical = Input.GetAxis("Vertical");
-//		Vector3 translate = new Vector3(moveHorizontal, moveVertical, 0.0f) * Time.deltaTime * MOVE_SPEED;
-//		transform.Translate(translate);
-
-
-		if (Input.GetKey ("joystick 1 button 0")) {
-			Debug.Log ("joystick 1 button 0");
-		}
-
-		if (Input.GetKey ("joystick 2 button 0")) {
-			Debug.Log ("joystick 2 button 0");
-		}
-
-		if (Input.GetKey (up))
-		{
-			transform.parent.gameObject.rigidbody2D.AddForceAtPosition(Vector3.up * Time.deltaTime * 100, transform.localPosition);
-		}
-		if (Input.GetKey (down))
-		{    
-			transform.parent.gameObject.rigidbody2D.AddForceAtPosition(Vector3.down * Time.deltaTime * 100, transform.localPosition);
-		}  
-		if (Input.GetKey (right))
-		{
-			transform.parent.gameObject.rigidbody2D.AddForceAtPosition(Vector3.right * Time.deltaTime * 100, transform.localPosition);
-		}
-		if (Input.GetKey (left))
-		{    
-			transform.parent.gameObject.rigidbody2D.AddForceAtPosition(Vector3.left * Time.deltaTime * 100, transform.localPosition);
-		}  
+		float moveHorizontal = Input.GetAxis("p" + player.ToString() + "_Horizontal_keyboard");
+		float moveVertical = Input.GetAxis("p" + player.ToString() + "_Vertical_keyboard");
+		Vector3 translate = new Vector3(moveHorizontal, moveVertical, 0.0f) * Time.deltaTime * MOVE_SPEED;
+		transform.Translate(translate);
 	}
 }
