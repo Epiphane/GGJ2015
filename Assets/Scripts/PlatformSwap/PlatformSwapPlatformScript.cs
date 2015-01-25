dd76909c589f57990ad68077da9cc7f2263269db
Aaron Jacobs <aaron@aaronmjacobs.com>
2015-01-24 21:45:30 -0800

    Swap crap

---
+++ Assets/Scripts/PlatformSwap/PlatformSwapPlatformScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSwapPlatformScript : MonoBehaviour {
+
+	private bool enabled;
+	private SpriteRenderer spriteRenderer;
+
+	// Use this for initialization
+	void Start () {
+		spriteRenderer = GetComponent<SpriteRenderer>();
+		enabled = Random.value > 0.5f;
+		swap();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void swap() {
+		enabled = !enabled;
+
+		Color color = spriteRenderer.color;
+		color.a = enabled ? 1.0f : 0.5f;
+		spriteRenderer.color = color;
+
+		collider2D.isTrigger = !enabled;
+	}
+}
---
+++ Assets/Scripts/PlatformSwap/PlatformSwapPlayerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlatformSwapPlayerScript : MonoBehaviour {
+
+	const float MOVE_SPEED = 5.0f;
+
+	public string player;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float horizAxis = Input.GetAxis(player + "_Horizontal_keyboard");
+		rigidbody2D.velocity = new Vector2(horizAxis * MOVE_SPEED, rigidbody2D.velocity.y);
+
+		float jump = Input.GetKey(KeyCode.Space) ? 50.0f : 0.0f;
+		rigidbody2D.AddForce(Vector2.up * jump);
+	}
+}
