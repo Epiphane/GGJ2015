07d685bb19a57e2f9f7f0786088d87213b3e3955
Aaron Jacobs <aaron@aaronmjacobs.com>
2015-01-24 17:32:56 -0800

    Add saboteur to flappy

--- Assets/Scripts/Flappybird/PipeScript.cs
+++ Assets/Scripts/Flappybird/PipeScript.cs
@@ -27,7 +27,7 @@ public class PipeScript : MonoBehaviour {
 		}
 	}
 
-	void OnTriggerEnter2D(Collider2D other) {
+	void OnCollisionEnter2D(Collision2D other) {
 		if (other.gameObject == bird1) {
 			bird1Script.OnHitPipe();
 		}
