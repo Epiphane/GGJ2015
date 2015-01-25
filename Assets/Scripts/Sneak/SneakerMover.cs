c70b391e741ce2e0b86e7f1a16630e21bd24f4f2
ElliotFiske <elliotfiske@gmail.com>
2015-01-25 02:22:04 -0800

    Movin around, sneaky style

---
+++ Assets/Scripts/Sneak/SneakerMover.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SneakerMover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GlobalInput.Meh(); // we'll do it live
+	}
+
+	public int player;
+	private const float MOVE_SPEED = 5;
+
+	// Update is called once per frame
+	void Update () {
+		float moveHorizontal =  GlobalInput.players[player].XAxis();
+		float moveVertical =   -GlobalInput.players[player].YAxis();
+		Vector3 translate = new Vector3(moveHorizontal, moveVertical, 0.0f) * Time.deltaTime * MOVE_SPEED;
+		transform.Translate(translate);
+	}
+}
